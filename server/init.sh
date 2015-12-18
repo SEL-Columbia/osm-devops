6c4b159d8dc9d8b2aea0901a76e417b73efa20d1
chrisnatali <chris.natali@gmail.com>
2015-12-18 16:47:53 -0500

    ensure db is up and add admin user if not already there.  Closes #5, #2

--- server/init.sh
+++ server/init.sh
@@ -2,7 +2,19 @@
 # setup osm website. This will be run on each container startup
 echo "*** running gridmaps init script"
 pushd /home/app/openstreetmap-website
+
+# wait for database (checked via rails config)
+while ! sudo -u app rails runner -e production 'exit(begin; ActiveRecord::Base.connection; rescue Exception; false; else true; end)'
+do
+  echo "waiting for database..."
+  sleep 1
+done
+
 sudo -u app RAILS_ENV=production rake db:migrate
+
+echo "adding admin user if not done..."
+sudo -u app rails runner -e production /home/app/add_admin.rb 
+
 sudo -u app RAILS_ENV=production rake assets:precompile
 
 # set the SECRET_KEY for prod on each startup
