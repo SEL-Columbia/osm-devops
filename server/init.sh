68f1cf835fd2d772f48925b0a27c93d28610cb5b
chrisnatali <chris.natali@gmail.com>
2016-01-13 17:54:17 -0500

    allow application.yml to be updated in container with iD editor key

--- server/init.sh
+++ server/init.sh
@@ -3,6 +3,15 @@
 echo "*** running gridmaps init script"
 pushd /home/app/openstreetmap-website
 
+# copy application.yml so that we can run rails
+# a hack, but this keeps the external file untouched
+if [[ ! -f /tmp/application.yml ]]; then
+    echo "osm configuration file must be mounted to /tmp/application.yml"
+	exit 1
+fi
+
+cp /tmp/application.yml config/application.yml
+
 # wait for database (checked via rails config)
 while ! sudo -u app rails runner -e production 'exit(begin; ActiveRecord::Base.connection; rescue Exception; false; else true; end)'
 do
@@ -18,11 +27,9 @@ sudo -u app rails runner -e production /home/app/add_admin.rb
 echo "registering id as client if not done..."
 sudo -u app rails runner -e production /home/app/add_id_client.rb 
 
-# sub the id client key into application.yml
-if [[ -f config/application.yml ]]; then 
-    id_key=`psql -h db -d osm -U postgres -tA -c "select key from client_applications where name='iD';"`
-    sed -i "s/<id_key>/$id_key/" config/application.yml
-fi
+# sub the iD client key into application.yml and copy into config dir
+id_key=`psql -h db -d osm -U postgres -tA -c "select key from client_applications where name='iD';"`
+sed -i "s/<id_key>/$id_key/" config/application.yml
 
 sudo -u app RAILS_ENV=production rake assets:precompile
 
